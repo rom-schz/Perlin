709e54e5a022cc1af5fcff24cc6ca1a31ec14558
Romain <Romain@DESKTOP-AORUS>
2023-03-20 11:38:39 +0100

    Fixed test dependencies, removed std::cout as it was irrelevant for the test to come.

--- UnitTest1D/UnitTest1D.cpp
+++ UnitTest1D/UnitTest1D.cpp
@@ -1,9 +1,9 @@
+#include <iostream>
+
 #include "pch.h"
-#include "CppUnitTest.h"
 
 #include "perlin.h"
-
-#include <iostream>
+#include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,7 +12,7 @@ namespace UnitTest1D
 	TEST_CLASS(UnitTest1D)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
 			std::vector<uint64_t> sizes = std::vector<uint64_t>();
@@ -20,10 +20,7 @@ namespace UnitTest1D
 			sizes.push_back(3);
 			sizes.push_back(3);
 
-			std::cout << "Computing grid... ";
 			Perlin::Perlin p = Perlin::Perlin(sizes);
-
-			std::cout << "Success!\n";
-
+		};
 	};
-}
+};
